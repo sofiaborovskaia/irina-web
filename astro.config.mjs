9e5d3e25a60809812892d23052aaafa533385aeb
Sofia Borovskaia <sofia.borovskaia@capgemini.com>
2025-07-17 22:23:30 +0100

    chore: add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 
 export default defineConfig({
-  site: "https://sofiaborovskaia.github.io",
+  site: "https://sofiaborovskaia.github.io/irina-web/",
   base: "/irina-web/",
 });
