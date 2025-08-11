e59bc8230d5f01fc4a3accbf2f7c4386bb338c9f
Sofia Borovskaia <sofia.borovskaia@capgemini.com>
2025-08-11 17:18:58 +0100

    feat: add reviews slider

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 
 export default defineConfig({
   site: "https://sofiaborovskaia.github.io/irina-web",
   base: "/irina-web/",
+  integrations: [react()],
 });
